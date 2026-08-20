d6bba9b242fbf0bbd3f5228a15e64df0463d7b2d
Quang Khai <madgeee69420@gmail.com>
2026-08-20 09:40:05 +0700

    bugs fixes: - revert the default deck to default (before i forgot to change the cannon cards to scout cards) - remove the glow shader from the attack line - now in wave system they have to check if get_tree() is not null

--- Script/WaveSystem/wave_system.gd
+++ Script/WaveSystem/wave_system.gd
@@ -89,7 +89,8 @@ func nextWaveStart():
 
 func on_enemy_removed() -> void:
 	print("ENEMY REMOVED!!")
-	await get_tree().create_timer(randf_range(0.1,0.5)).timeout 
+	if get_tree():
+		await get_tree().create_timer(randf_range(0.1,0.5)).timeout 
 	# ^ this is to prevent the script from not being to check the last enemy when the 2 last enemies die at the same time.
 	check_final_wave_victory()
 
